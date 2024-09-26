cdd39a8dd01596b7ad761f201cd328f6e1fb454a
Alexandre Neto <alegneto@hotmail.com>
2024-09-26 14:05:58 -0300

    feat: implementar componentização e tailwindcss

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,29 +1,19 @@
-<h1>Listagem dos Suportes</h1>
+@extends('admin.layouts.app')
 
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
 
-<table>
-    <thead>
-        <th>assunto</th>
-        <th>status</th>
-        <th>descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">ver</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('title', 'Fórum')
+
+
+@section('header')
+@include('admin.supports.partials.header')
+@endsection
+
+
+@section('content')
+@include('admin.supports.partials.content')
 
 <x-pagination
     :paginator="$supports"
     :appends="$filters" />
+
+@endsection
